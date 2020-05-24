78fbeef3e4e723e20b81b37852ecfaef53f2efd4
dpkprmr <38376323+dpkprmr@users.noreply.github.com>
2020-05-24 17:33:49 +0530

    Renamed the project from Tuutorial to Tutorial

--- Restapi_Tutorial/asgi.py
+++ Restapi_Tutorial/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Restapi_Tuutorial project.
+ASGI config for Restapi_Tutorial project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restapi_Tuutorial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restapi_Tutorial.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restapi_Tuutorial.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restapi_Tutorial.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
